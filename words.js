98c6353b33aee1e9b62fe4020f011f3cac01025f
nomad447686 <zavierlam@thevapital.com>
2025-10-16 20:26:45 +0800

    Create words.js

---
+++ words.js
@@ -0,0 +1,54 @@
+const words = [
+  {
+    chinese: "校車",
+    english: "school bus",
+    image: "https://openmoji.org/data/color/svg/1F68C.svg"
+  },
+  {
+    chinese: "書包",
+    english: "school bag",
+    image: "https://openmoji.org/data/color/svg/1F392.svg"
+  },
+  {
+    chinese: "鉛筆",
+    english: "pencil",
+    image: "https://openmoji.org/data/color/svg/270F.svg"
+  },
+  {
+    chinese: "橡皮擦",
+    english: "eraser",
+    image: "https://openmoji.org/data/color/svg/1F9F9.svg"
+  },
+  {
+    chinese: "教室",
+    english: "classroom",
+    image: "https://openmoji.org/data/color/svg/1F3EB.svg"
+  },
+  {
+    chinese: "黑板",
+    english: "blackboard",
+    image: "https://openmoji.org/data/color/svg/1F5A5.svg"
+  },
+  {
+    chinese: "老師",
+    english: "teacher",
+    image: "https://openmoji.org/data/color/svg/1F469-200D-1F3EB.svg"
+  },
+  {
+    chinese: "學生",
+    english: "student",
+    image: "https://openmoji.org/data/color/svg/1F468-200D-1F393.svg"
+  },
+  {
+    chinese: "書本",
+    english: "book",
+    image: "https://openmoji.org/data/color/svg/1F4D6.svg"
+  },
+  {
+    chinese: "課桌",
+    english: "desk",
+    image: "https://openmoji.org/data/color/svg/1F4C4.svg"
+  }
+  // ...你可以繼續加更多
+];
+add words.js
